35275b4d2c400197be06b9cccec81a8f4b38f918
CARLOS DANIEL CARDENAS MALPICA <L23580450@reynosa.tecnm.mx>
2023-11-20 22:20:43 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Practica_1__unidad_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            btnAbrir = new Button();
+            txtMonto = new TextBox();
+            txtCliente = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            btnRetiro = new Button();
+            btnDeposito = new Button();
+            groupBox3 = new GroupBox();
+            btnNuevo = new Button();
+            txtSaldo = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            lstRetiros = new ListBox();
+            label3 = new Label();
+            lstDepositos = new ListBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnAbrir);
+            groupBox1.Controls.Add(txtMonto);
+            groupBox1.Controls.Add(txtCliente);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox1.Location = new Point(62, 39);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(392, 103);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ingrese los Datos";
+            // 
+            // btnAbrir
+            // 
+            btnAbrir.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnAbrir.Location = new Point(256, 44);
+            btnAbrir.Name = "btnAbrir";
+            btnAbrir.Size = new Size(113, 23);
+            btnAbrir.TabIndex = 4;
+            btnAbrir.Text = "Abrir Cuenta";
+            btnAbrir.UseVisualStyleBackColor = true;
+            btnAbrir.Click += btnAbrir_Click;
+            // 
+            // txtMonto
+            // 
+            txtMonto.Location = new Point(101, 65);
+            txtMonto.Name = "txtMonto";
+            txtMonto.Size = new Size(107, 29);
+            txtMonto.TabIndex = 3;
+            // 
+            // txtCliente
+            // 
+            txtCliente.Location = new Point(100, 25);
+            txtCliente.Name = "txtCliente";
+            txtCliente.Size = new Size(108, 29);
+            txtCliente.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(17, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Monto";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(17, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Cliente";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnRetiro);
+            groupBox2.Controls.Add(btnDeposito);
+            groupBox2.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox2.Location = new Point(60, 148);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(394, 84);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Trasaccion";
+            // 
+            // btnRetiro
+            // 
+            btnRetiro.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            btnRetiro.Location = new Point(208, 28);
+            btnRetiro.Name = "btnRetiro";
+            btnRetiro.Size = new Size(93, 30);
+            btnRetiro.TabIndex = 1;
+            btnRetiro.Text = "Retiro";
+            btnRetiro.UseVisualStyleBackColor = true;
+            btnRetiro.Click += btnRetiro_Click;
+            // 
+            // btnDeposito
+            // 
+            btnDeposito.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            btnDeposito.Location = new Point(41, 28);
+            btnDeposito.Name = "btnDeposito";
+            btnDeposito.Size = new Size(99, 30);
+            btnDeposito.TabIndex = 0;
+            btnDeposito.Text = "Deposito";
+            btnDeposito.UseVisualStyleBackColor = true;
+            btnDeposito.Click += btnDeposito_Click;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(btnNuevo);
+            groupBox3.Controls.Add(txtSaldo);
+            groupBox3.Controls.Add(label5);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Controls.Add(lstRetiros);
+            groupBox3.Controls.Add(label3);
+            groupBox3.Controls.Add(lstDepositos);
+            groupBox3.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox3.Location = new Point(62, 247);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(392, 191);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Historial";
+            // 
+            // btnNuevo
+            // 
+            btnNuevo.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            btnNuevo.Location = new Point(279, 144);
+            btnNuevo.Name = "btnNuevo";
+            btnNuevo.Size = new Size(75, 23);
+            btnNuevo.TabIndex = 6;
+            btnNuevo.Text = "Nuevo";
+            btnNuevo.UseVisualStyleBackColor = true;
+            btnNuevo.Click += btnNuevo_Click;
+            // 
+            // txtSaldo
+            // 
+            txtSaldo.Location = new Point(278, 112);
+            txtSaldo.Name = "txtSaldo";
+            txtSaldo.Size = new Size(84, 27);
+            txtSaldo.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(278, 92);
+            label5.Name = "label5";
+            label5.Size = new Size(49, 17);
+            label5.TabIndex = 4;
+            label5.Text = "Sueldo";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(164, 29);
+            label4.Name = "label4";
+            label4.Size = new Size(44, 17);
+            label4.TabIndex = 3;
+            label4.Text = "Retiro";
+            // 
+            // lstRetiros
+            // 
+            lstRetiros.FormattingEnabled = true;
+            lstRetiros.ItemHeight = 20;
+            lstRetiros.Location = new Point(140, 53);
+            lstRetiros.Name = "lstRetiros";
+            lstRetiros.Size = new Size(96, 104);
+            lstRetiros.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(29, 29);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 17);
+            label3.TabIndex = 1;
+            label3.Text = "Deposito";
+            // 
+            // lstDepositos
+            // 
+            lstDepositos.FormattingEnabled = true;
+            lstDepositos.ItemHeight = 20;
+            lstDepositos.Location = new Point(17, 53);
+            lstDepositos.Name = "lstDepositos";
+            lstDepositos.Size = new Size(94, 104);
+            lstDepositos.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Gestion de cuenta de ahorros";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label2;
+        private Label label1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private Button btnAbrir;
+        private TextBox txtMonto;
+        private TextBox txtCliente;
+        private Button btnRetiro;
+        private Button btnDeposito;
+        private Label label5;
+        private Label label4;
+        private ListBox lstRetiros;
+        private Label label3;
+        private ListBox lstDepositos;
+        private Button btnNuevo;
+        private TextBox txtSaldo;
+    }
+}
